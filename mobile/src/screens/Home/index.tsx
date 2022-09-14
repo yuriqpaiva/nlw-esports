b421295955d2b71f0a9312ca0d29c696b3caabde
Yuri <yuriqpaiva@gmail.com>
2022-09-13 22:45:11 -0300

    Add mobile layout

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import { FlatList, Image, View } from "react-native";
+import { styles } from "./styles";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Header } from "../../components/Header";
+import { GameCard } from "../../components/GameCard";
+import { GAMES } from "../../utils/games";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+
+      <Header
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => <GameCard data={item} />}
+      />
+    </View>
+  );
+}
