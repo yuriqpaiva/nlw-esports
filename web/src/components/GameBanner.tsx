d5242139178018a2880a57bf6dc8bf07e214054b
Yuri <yuriqpaiva@gmail.com>
2022-09-17 14:31:23 -0300

    Add web image hover animation

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -6,8 +6,11 @@ interface GameBannerProps {
 
 export function GameBanner({ bannerUrl, title, adsCount }: GameBannerProps) {
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
-      <img src={bannerUrl} alt="" />
+    <a href="" className="relative group overflow-hidden rounded-lg">
+      <img
+        src={bannerUrl}
+        className="group-hover:scale-110 transition-all duration-300 object-contain w-full h-full"
+      />
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{title}</strong>
         <span className="text-zinc-300 text-sm block">
