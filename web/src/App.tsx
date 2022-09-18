479775794832dd730d363151a55ae4424c10c524
Yuri <yuriqpaiva@gmail.com>
2022-09-17 22:20:25 -0300

    Add game slider responsiveness

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,6 +10,7 @@ import { CaretLeft, CaretRight } from "phosphor-react";
 import Lottie from "react-lottie";
 import gameControlLottie from "./assets/lotties/game-control.json";
 import { KeenSliderOptions } from "keen-slider";
+import { GameSlider } from "./components/GameSlider";
 
 interface Game {
   id: string;
@@ -22,36 +23,15 @@ interface Game {
 
 function App() {
   const [games, setGames] = useState<Game[]>([]);
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [options, setOptions] = useState<KeenSliderOptions>({});
-
-  const [sliderRef, instanceRef] = useKeenSlider(options);
 
   useEffect(() => {
     axios("http://localhost:3333/games").then(({ data }) => {
       setGames(data);
-      setOptions({
-        slideChanged(slider: any) {
-          setCurrentSlide(slider.track.details.rel);
-        },
-        created() {
-        },
-        renderMode: "precision",
-        slides: {
-          origin: "auto",
-          perView: 6,
-          spacing: 24,
-        },
-      });
     });
   }, []);
 
-  useEffect(() => {
-    instanceRef.current?.update();
-  }, [games]);
-
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20 px-14">
       <img src={logoImg} />
 
       <h1 className="text-6xl text-white font-black mt-20">
@@ -61,56 +41,9 @@ function App() {
         </span>{" "}
         está aqui.
       </h1>
-      <div className="relative">
-        <>
-          <div
-            ref={sliderRef}
-            className="mt-16 relative max-w-[1200px] flex overflow-hidden"
-          >
-            {games.map((game, index) => {
-              return (
-                <GameBanner
-                  key={game.id}
-                  bannerUrl={game.bannerUrl}
-                  title={game.title}
-                  adsCount={game._count.ads}
-                  index={index}
-                />
-              );
-            })}
-          </div>
-          <>
-            <button
-              className="absolute -left-16 top-[calc(4rem+90px)]"
-              onClick={(e: any) => {
-                e.stopPropagation() || instanceRef.current?.prev();
-              }}
-            >
-              <CaretLeft
-                size={48}
-                className={`text-zinc-300 transition-opacity ${
-                  currentSlide === 0 && "opacity-50"
-                }`}
-              />
-            </button>
-            <button
-              className="absolute -right-16 top-[calc(4rem+90px)]"
-              onClick={(e: any) => {
-                e.stopPropagation() || instanceRef.current?.next();
-              }}
-            >
-              <CaretRight
-                size={48}
-                className={`text-zinc-300 ${
-                  instanceRef.current &&
-                  instanceRef.current?.track.details?.maxIdx === currentSlide &&
-                  "opacity-50"
-                }`}
-              />
-            </button>
-          </>
-        </>
-      </div>
+      
+      <GameSlider games={games} />
+
       {games.length === 0 && (
         <div className="mt-16 h-[240px]">
           <Lottie
---
+++ web/src/components/GameSlider.tsx
@@ -0,0 +1,101 @@
+import { KeenSliderOptions } from "keen-slider";
+import { useKeenSlider } from "keen-slider/react";
+import { CaretLeft, CaretRight } from "phosphor-react";
+import { useEffect, useState } from "react";
+import { GameBanner } from "./GameBanner";
+
+interface GameSliderProps {
+  games: {
+    id: string;
+    title: string;
+    bannerUrl: string;
+    _count: {
+      ads: number;
+    };
+  }[];
+}
+
+export function GameSlider({ games }: GameSliderProps) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const [sliderRef, instanceRef] = useKeenSlider({
+    slideChanged(slider: any) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {},
+    breakpoints: {
+      "(max-width: 640px)": {
+        slides: { perView: 1, spacing: 24 },
+      },
+      "(min-width: 640px)": {
+        slides: { perView: 2, spacing: 24 },
+      },
+      "(min-width: 768px)": {
+        slides: { perView: 4, spacing: 24 },
+      },
+      "(min-width: 1024px)": {
+        slides: { perView: 5, spacing: 24 },
+      },
+      "(min-width: 1280px)": {
+        slides: { perView: 6, spacing: 24 },
+      },
+    },
+    slides: {
+      perView: 6,
+      spacing: 24,
+    },
+  });
+
+  useEffect(() => {
+    instanceRef.current?.update();
+  }, [games]);
+
+  return (
+    <div className="relative w-full flex justify-center">
+      <div
+        ref={sliderRef}
+        className="mt-16 relative max-w-[1200px] flex overflow-hidden w-full "
+      >
+        {games.map((game, index) => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+              index={index}
+            />
+          );
+        })}
+      </div>
+      <button
+        className="absolute -left-14 top-[calc(4rem+90px)]"
+        onClick={(e: any) => {
+          e.stopPropagation() || instanceRef.current?.prev();
+        }}
+      >
+        <CaretLeft
+          size={48}
+          className={`text-zinc-300 transition-opacity ${
+            currentSlide === 0 && "opacity-50"
+          }`}
+        />
+      </button>
+      <button
+        className="absolute -right-14 top-[calc(4rem+90px)]"
+        onClick={(e: any) => {
+          e.stopPropagation() || instanceRef.current?.next();
+        }}
+      >
+        <CaretRight
+          size={48}
+          className={`text-zinc-300 ${
+            instanceRef.current &&
+            instanceRef.current?.track.details?.maxIdx === currentSlide &&
+            "opacity-50"
+          }`}
+        />
+      </button>
+    </div>
+  );
+}
